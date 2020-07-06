aeb80f6d9e9c8e0d856a431a52cc2f14dd6b4170
AlenaMikalaeynaLisava <63248586+AlenaMikalaeynaLisava@users.noreply.github.com>
2020-07-06 19:17:20 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,30 +1,22 @@
-
 function RandomNumberGenerator(min, max, length){
   if(length>(max-min)){
     return;
   }
 const a=[];
 function  randomInteger() {
-  let i=0;
   let rand1 = min + Math.random() * (max + 1 - min);
   let rand =Math.floor(rand1);
-  createArray();
-  function createArray() {
-  if(i==length){
+  if(a.length==length){
     return;
   } else {
-    if(rand!==a[i] && a[i]){
-      i++;
-      createArray();
-    } else {
-      a[i]=rand;
-      randomInteger();
+    if(!a.includes(rand)){
+      a.push(rand);
     }
-  }
+    randomInteger();
 }
 }
 randomInteger();
 return(a);
 }
 
-console.log(RandomNumberGenerator(1, 25, 15));
+console.log(RandomNumberGenerator(1, 25, 23));
